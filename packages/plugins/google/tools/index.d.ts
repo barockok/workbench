49d8bdd399d69870b6ca1df7555e9c16c8e7bd56
Zidni Mubarok <zidmubarock@gmail.com>
2026-05-27 17:29:59 +0700

    chore: ignore generated build artifacts (.turbo, plugin js/d.ts)

--- packages/plugins/google/tools/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { sendEmail, listEmails, getEmail } from "./gmail";
-export { listFiles, createFolder } from "./drive";
-export { readSheet, writeSheet } from "./sheets";
-export { listEvents, createEvent } from "./calendar";
-export { generateContent } from "./gemini";
